df9f9178a1ea02ce9b46f39acd891df75b57afc4
bulka <jej07267@gmail.com>
2024-10-24 16:18:35 +0300

    - GBoot template

---
+++ src/gboot/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define U0 void
+#define U8 unsigned char
+#define U16 unsigned short int
+#define U32 unsigned int
+#define U64 unsigned long long int
+
+#define I8 char
+#define I16 short int
+#define I32 int
+#define I64 long long int
+
+I32 main(I32 argc, I8** argv) {
+  printf("GBoot 1.0\n");
+  return 0;
+}
